53b5af5093179562f476b72851db21a3191251df
wyattadkins@icloud.com <wyattadkins@icloud.com>
2022-10-25 22:33:59 -0500

    p

--- functionsExercise.js
+++ functionsExercise.js
@@ -2,13 +2,40 @@
 function fozziebear(){
     console.log(`Wocka Wocka!`);
 }
+
 // 2
 function beaker(speak){
     console.log(`${speak}, ${speak}, ${speak}!`);
 }
 beaker(`meep`);
+
 // 3
-function muppetshow(speak){
-    console.log(`${speak}, ${speak}, ${speak}!`);
+function muppetshow (a,b){
+    console.log(`${a} ${b} It's time to play the music. It's time to light the lights.`);
 }
-beaker(`meep`);
+muppetshow(`muppet`, `show`);
+
+// 4
+function Kermit(){
+    console.log(`Kermit The Frog?`);
+}
+
+// 5
+
+// 6
+function manOrMuppet(){
+    console.log(`Am I a man or am I a Muppet`);
+}
+
+// 7
+const rainbowConnection = [
+    'someday we\'ll find initials. The Rainbow Connection'
+  ];
+  
+  console.log(rainbowConnection.map(rainbowConnection => rainbowConnection.length));
+
+// 8 no
+
+// 9 no
+
+// do not commit yet, finish in morning
