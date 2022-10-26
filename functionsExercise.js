ed7b50d6a0d59ec0b4ccb04cf5ca67b50b888ef6
wyattadkins@icloud.com <wyattadkins@icloud.com>
2022-10-26 11:29:58 -0500

    muppets in class

--- functionsExercise.js
+++ functionsExercise.js
@@ -1,41 +1,65 @@
+const b = '='.repeat(25);
+
 // 1
 function fozziebear(){
     console.log(`Wocka Wocka!`);
 }
+fozziebear();
+
+console.log(b);
 
 // 2
 function beaker(speak){
-    console.log(`${speak}, ${speak}, ${speak}!`);
+    console.log(speak.repeat(4));
 }
+
 beaker(`meep`);
 
 // 3
 function muppetshow (a,b){
+    if (a === 'muppet' && b=== 'show'){
     console.log(`${a} ${b} It's time to play the music. It's time to light the lights.`);
-}
+} }
 muppetshow(`muppet`, `show`);
 
+console.log(b);
 // 4
-function Kermit(){
-    console.log(`Kermit The Frog?`);
+function kermit(){
+    return `Kermit The Frog`;
 }
+kermit()
+console.log(kermit());
+
+console.log(b);
 
 // 5
 
-// 6
-function manOrMuppet(){
-    console.log(`Am I a man or am I a Muppet`);
+function muppetShowSeasons(seasons){
+    if (seasons === 5){
+        return true
+    } else {
+        return false
+    }
 }
 
+console.log(muppetShowSeasons(5));
+
+console.log(b);
+// 6
+const manOrMuppet = function(){
+    console.log(`Am I a man or am I a Muppet`);
+}
+manOrMuppet();
+
 // 7
-const rainbowConnection = [
-    'someday we\'ll find initials. The Rainbow Connection'
-  ];
+rainbowConnection = () =>
+    'someday we\'ll find it. The Rainbow Connection the lovers the dreamers and me'
+  ;
   
   console.log(rainbowConnection.map(rainbowConnection => rainbowConnection.length));
 
 // 8 no
 
-// 9 no
+// 9 yes
 
 // do not commit yet, finish in morning
