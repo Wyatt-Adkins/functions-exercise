ac579835f1885840ddcfb9e7c1ea95ce6c15d50c
wyattadkins@icloud.com <wyattadkins@icloud.com>
2022-10-25 21:43:38 -0500

    muppet show is cool :)

---
+++ functionsExercise.js
@@ -0,0 +1,14 @@
+// 1
+function fozziebear(){
+    console.log(`Wocka Wocka!`);
+}
+// 2
+function beaker(speak){
+    console.log(`${speak}, ${speak}, ${speak}!`);
+}
+beaker(`meep`);
+// 3
+function muppetshow(speak){
+    console.log(`${speak}, ${speak}, ${speak}!`);
+}
+beaker(`meep`);
